1416efb23e4cd30013db232b540fda5bbbfc2600
HELEN HARMAN <hlharman@bellsouth.net>
2020-10-26 01:35:40 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 def valid_move?(board, index)
-if index.between?(0, 8)
-  return true 
+if not index.between?(0, 8)
+  return false
   else puts "this is an invalid move"
 if board[index] == " " || board[index] == "" || board[index] == nil 
     return true 
