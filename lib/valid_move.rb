90ca8599cc404d60fc378690bb0f7a1b35700f5d
HELEN HARMAN <hlharman@bellsouth.net>
2020-10-26 00:59:02 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 def valid_move(board, index)
 if index between? (0,8)
-  puts "this is an invalid mmove"
+else puts "this is an invalid mmove"
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
  if board[index] == " " || board[index] == "" || board[index] == nil 
